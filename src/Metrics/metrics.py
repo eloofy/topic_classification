32a8d1f8da46f6c268b2c51eaa1d6dbd5f01af0c
anatoly <t.vlasov23@mail.ru>
2024-03-16 14:15:36 +0300

    Add metrics module

---
+++ src/Metrics/metrics.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from torchmetrics import F1Score, MetricCollection, Precision, Recall
+
+
+def get_metrics(**kwargs: Any) -> MetricCollection:
+    """
+    Get metrics
+
+    :param kwargs: keyword arguments
+    :return: metric collection
+    """
+    return MetricCollection(
+        {
+            'f1': F1Score(**kwargs),
+            'precision': Precision(**kwargs),
+            'recall': Recall(**kwargs),
+        },
+    )
